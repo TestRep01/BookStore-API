4cedb1f9b70a6d12e485a2cf8f222fbd8917daa9
aden bruwer <phoenix77774@outlook.com>
2021-02-25 07:58:27 +0200

    base

---
+++ Contracts/ILoggerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+        void Logwarn(string message);
+        void Logdebug(string message);
+        void Logerror(string message);
+    }
+}
---
+++ Services/LoggerService.cs
@@ -0,0 +1,34 @@
+﻿using BookStore_API.Contracts;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void Logdebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void Logerror(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Logwarn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
